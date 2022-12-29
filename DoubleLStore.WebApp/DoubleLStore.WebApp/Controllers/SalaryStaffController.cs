cc194803549f8d7e203afa85cf911342122403b0
ngoclam2000 <lam.bui@dotnets.org>
2022-12-29 09:46:47 +0700

    add code

--- DoubleLStore.WebApp/DoubleLStore.WebApp/Controllers/SalaryStaffController.cs
+++ DoubleLStore.WebApp/DoubleLStore.WebApp/Controllers/SalaryStaffController.cs
@@ -170,7 +170,15 @@ namespace DoubleLStore.WebApp.Controllers
             try
             {
                 findsalarystaff.isWorking = false;
-                var monthyear = (DateTime.Now.Month+1).ToString() + "/" + DateTime.Now.Year.ToString();
+                var monthyear = "";
+                if (DateTime.Now.Month == 12)
+                {
+                    monthyear = "1" + "/" + (DateTime.Now.Year + 1).ToString();
+                }
+                else
+                {
+                    monthyear = (DateTime.Now.Month + 1).ToString() + "/" + DateTime.Now.Year.ToString();
+                }
                 SalaryStaff salaryStaffs = new SalaryStaff();
                 salaryStaffs.NumberOfWorking = 0;
                 salaryStaffs.Salary = findsalarystaff.Salary;
@@ -181,8 +189,8 @@ namespace DoubleLStore.WebApp.Controllers
                 salaryStaffs.StaffId = findsalarystaff.StaffId;
                 _context.Add(salaryStaffs);
                 await _context.SaveChangesAsync();
-               
-            
+
+
 
 
             }
@@ -192,5 +200,55 @@ namespace DoubleLStore.WebApp.Controllers
             }
             return Ok(new Response { Status = 200, Message = "Tính lương tháng này thành công", Data = findsalarystaff });
         }
+        //public async Task<IActionResult> PayForMonth([FromBody] PayForMonthRequest request)
+        //{
+        //    var RoleId = "";
+        //    Request.Headers.TryGetValue("Authorization", out var tokenheaderValue);
+        //    JwtSecurityToken token = null;
+        //    try
+        //    {
+        //        token = _jwtAuthenticationManager.GetInFo(tokenheaderValue);
+        //    }
+        //    catch (IndexOutOfRangeException e)
+        //    {
+        //        return BadRequest(new Response { Status = 400, Message = "Không xác thực được người dùng" });
+        //    }
+        //    RoleId = token.Claims.First(claim => claim.Type == "RoleId").Value;
+        //    if (RoleId != "1")
+        //        return BadRequest(new Response { Status = 400, Message = "Không có quyền!, vui lòng đăng nhập với tài khoản admin" });
+
+
+        //    var findsalarystaff = await _context.SalaryStaffs.FindAsync(request.Id);
+        //    if (findsalarystaff == null)
+        //    {
+        //        return NotFound(new Response { Status = 404, Message = "Bảng lương không tồn tại không tồn tại" });
+        //    }
+
+
+        //    try
+        //    {
+        //        findsalarystaff.isWorking = false;
+        //        var monthyear = (DateTime.Now.Month+1).ToString() + "/" + DateTime.Now.Year.ToString();
+        //        SalaryStaff salaryStaffs = new SalaryStaff();
+        //        salaryStaffs.NumberOfWorking = 0;
+        //        salaryStaffs.Salary = findsalarystaff.Salary;
+        //        salaryStaffs.ListDayWorking = "";
+        //        salaryStaffs.Month = monthyear;
+        //        salaryStaffs.SalaryOfThisMonth = "";
+        //        salaryStaffs.isWorking = true;
+        //        salaryStaffs.StaffId = findsalarystaff.StaffId;
+        //        _context.Add(salaryStaffs);
+        //        await _context.SaveChangesAsync();
+
+
+
+
+        //    }
+        //    catch (IndexOutOfRangeException e)
+        //    {
+        //        return BadRequest(new Response { Status = 400, Message = e.ToString() });
+        //    }
+        //    return Ok(new Response { Status = 200, Message = "Tính lương tháng này thành công", Data = findsalarystaff });
+        //}
     }
 }
